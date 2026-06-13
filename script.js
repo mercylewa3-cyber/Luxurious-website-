49ea2f6c82766e6260457ae6a650eb665f7213cf
mercylewa3-cyber <mercylewa3@gmail.com>
2026-06-13 14:09:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const hiddenElements = document.querySelectorAll(".fade-up");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+
+    });
+
+});
+
+hiddenElements.forEach((element) => {
+    observer.observe(element);
+});
+
+
 const button = document.getElementById("exploreBtn");
 
 button.addEventListener("click", () => {
